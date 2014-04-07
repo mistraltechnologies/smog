5ce32eadd54bb45f1b6b0c18df94e336babfa58f
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-07 22:15:11 +0100

    Correct minor inconsistency in javadoc formatting.

--- src/main/java/com/mistraltech/smog/PathProvider.java
+++ src/main/java/com/mistraltech/smog/PathProvider.java
@@ -2,11 +2,12 @@ package com.mistraltech.smog;
 
 /**
  * Implemented by classes that are capable of providing a path.
- * <p>
+ * <p/>
  * No interpretation is put on the meaning of the path, nor its format which
  * is represented simply as a String.
  */
 public interface PathProvider {
+
     /**
      * Gets the path.
      *
