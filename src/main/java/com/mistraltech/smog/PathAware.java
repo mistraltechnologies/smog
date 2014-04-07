ab7a4ad61a7191a1b540400ffacf6b86ec935a19
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-07 22:15:11 +0100

    Correct minor inconsistency in javadoc formatting.

--- src/main/java/com/mistraltech/smog/PathAware.java
+++ src/main/java/com/mistraltech/smog/PathAware.java
@@ -2,11 +2,12 @@ package com.mistraltech.smog;
 
 /**
  * Implemented by classes that expect to be provided with some path.
- * <p>
+ * <p/>
  * This is useful where neither the path nor the provider of that path is known
  * at object construction.
  */
 public interface PathAware {
+
     /**
      * Assign a path provider.
      *
