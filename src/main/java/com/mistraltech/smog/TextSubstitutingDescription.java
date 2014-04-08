8c66a7b551f1e86226ad49496e1f1dcff3c85d80
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-08 19:12:59 +0100

    Fix code formatting issues in main source code.

--- src/main/java/com/mistraltech/smog/TextSubstitutingDescription.java
+++ src/main/java/com/mistraltech/smog/TextSubstitutingDescription.java
@@ -6,20 +6,17 @@ import org.hamcrest.StringDescription;
  * A type of Description that substitutes parts of the description matching a regular expression
  * with some other text.
  */
-class TextSubstitutingDescription extends StringDescription
-{
+class TextSubstitutingDescription extends StringDescription {
     final String regex;
     final String replacement;
 
-    public TextSubstitutingDescription(String regex, String replacement)
-    {
+    public TextSubstitutingDescription(String regex, String replacement) {
         this.regex = regex;
         this.replacement = replacement;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return super.toString().replaceAll(regex, replacement);
     }
 }
