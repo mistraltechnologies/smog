396bb59a0bac55f02126ef14c0de90cea5eb4425
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-08 21:10:40 +0100

    Remove dependence on SLF4J for consumers where logging is not required.
    Logging is now optional. Extend CompositePropertyMatcher where logging is
    not required. In this case SLF4J is not a required dependency. Where
    logging is required, extend LoggingCompositePropertyMatcher -
    there will then be a runtime dependency on SLF4J. Consumers can
    instead write their own extension to CompositePropertyMatcher to use
    alternative logging libraries.

---
+++ src/main/java/com/mistraltech/smog/core/LoggingCompositePropertyMatcher.java
@@ -0,0 +1,22 @@
+package com.mistraltech.smog.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A type of {@link CompositePropertyMatcher} that logs mismatches to an SLF4J logger.
+ *
+ * @param <T> type of matchable target object
+ */
+public abstract class LoggingCompositePropertyMatcher<T> extends CompositePropertyMatcher<T> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompositePropertyMatcher.class);
+
+    protected LoggingCompositePropertyMatcher(String matchedObjectDescription) {
+        super(matchedObjectDescription);
+    }
+
+    @Override
+    protected void writeLog(String text) {
+        LOGGER.debug(text);
+    }
+}
