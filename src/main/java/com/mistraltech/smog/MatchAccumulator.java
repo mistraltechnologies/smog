f4a30a7ab5de58cc7d78fc602165f110fb092073
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-08 19:08:59 +0100

    Correct and improve javadoc comments in MatchAccumulator.

--- src/main/java/com/mistraltech/smog/MatchAccumulator.java
+++ src/main/java/com/mistraltech/smog/MatchAccumulator.java
@@ -33,7 +33,12 @@ public final class MatchAccumulator {
     }
 
     /**
-     * Factory method for use by non-inheriting matchers.
+     * Factory method for use by inheriting matchers - that is, matchers that inherit some of their
+     * properties from a superclass.
+     * <p/>
+     * In this case it is desirable to construct the MatchAccumulator with
+     * an indicator of whether preceding property matches (performed by the matcher superclass) have
+     * passed or failed.
      *
      * @param mismatchDescription the description object used for recording the mismatch description
      * @param currentlyMatching a flag to indicate if, in the case that this instance is participating as
@@ -44,6 +49,15 @@ public final class MatchAccumulator {
         return new MatchAccumulator(mismatchDescription, currentlyMatching);
     }
 
+    /**
+     * Invokes the {@link Matcher#matches(java.lang.Object)} method on the supplied matcher against a given target object
+     * and appends relevant text to the mismatch description if the match fails.
+     *
+     * @param matcher the matcher to invoke
+     * @param item the target object to be matched
+     * @param <P> the type of item to be matched
+     * @return this instance, to allow multiple calls to be chained
+     */
     public <P> MatchAccumulator matches(Matcher<? super P> matcher, P item) {
         boolean localMatches = matcher.matches(item);
 
