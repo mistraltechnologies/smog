7a1d432cbce83a76d376d7f10cb9f16cf99d0c25
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-04-08 19:12:59 +0100

    Fix code formatting issues in main source code.

--- src/main/java/com/mistraltech/smog/MatchAccumulator.java
+++ src/main/java/com/mistraltech/smog/MatchAccumulator.java
@@ -8,7 +8,7 @@ import org.hamcrest.Matcher;
  * coordinates the building of a cumulative mismatch description for those that fail.
  */
 public final class MatchAccumulator {
-    public static final String    MISMATCH_JOINING_STRING = "\n     and: ";
+    public static final String MISMATCH_JOINING_STRING = "\n     and: ";
 
     private boolean matches = true;
     private Description mismatchDescription;
