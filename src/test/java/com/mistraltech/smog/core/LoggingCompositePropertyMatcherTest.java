7ee345fc6a33afba40e0bfc3e0842fb5495535f9
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-05-07 21:52:36 +0100

    Add unit tests and implementation of LoggingCompositePropertyMatcher.
    This now uses TextSubstitutingDescription to replace the default description
    with one that has no newlines and is therefore more suitable to log output.
    The test code uses a technique described in
    http://jsoftbiz.wordpress.com/2011/11/29/unit-testing-asserting-that-a-line-was-logged-by-logback/
    to test log output.

---
+++ src/test/java/com/mistraltech/smog/core/LoggingCompositePropertyMatcherTest.java
@@ -0,0 +1,71 @@
+package com.mistraltech.smog.core;
+
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class LoggingCompositePropertyMatcherTest {
+
+    @Test
+    public void messageIsLoggedWhenMatchFails() {
+        CompositePropertyMatcher<Item> cpm = new LoggingCompositePropertyMatcher<Item>("parent");
+        addPropertyMatcher(cpm, "val1", "foo");
+        addPropertyMatcher(cpm, "val2", "foo");
+
+        final Appender<ILoggingEvent> mockAppender = createMockAppender();
+
+        cpm.matches(new Item("bar"));
+
+        ArgumentCaptor<ILoggingEvent> loggingEventCaptor = ArgumentCaptor.forClass(ILoggingEvent.class);
+        verify(mockAppender).doAppend(loggingEventCaptor.capture());
+
+        assertThat("message", loggingEventCaptor.getValue().getFormattedMessage(),
+                endsWith("didn't match - val1 was \"bar\" (expected \"foo\") and val2 was \"bar\" (expected \"foo\")"));
+        assertEquals("level", Level.DEBUG, loggingEventCaptor.getValue().getLevel());
+    }
+
+    private void addPropertyMatcher(CompositePropertyMatcher<Item> cpm, String propertyName, String value) {
+        PropertyMatcher<String> propertyMatcher = new ReflectingPropertyMatcher<String>(propertyName);
+        propertyMatcher.setMatcher(equalTo(value));
+        cpm.addPropertyMatchers(propertyMatcher);
+    }
+
+    private Appender<ILoggingEvent> createMockAppender() {
+        @SuppressWarnings("unchecked")
+        final Appender<ILoggingEvent> mockAppender = mock(Appender.class);
+
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.addAppender(mockAppender);
+
+        return mockAppender;
+    }
+
+    private static class Item {
+        private String value;
+
+        private Item(String value) {
+            this.value = value;
+        }
+
+        @SuppressWarnings("UnusedDeclaration")
+        public String getVal1() {
+            return value;
+        }
+
+        @SuppressWarnings("UnusedDeclaration")
+        public String getVal2() {
+            return value;
+        }
+    }
+}
