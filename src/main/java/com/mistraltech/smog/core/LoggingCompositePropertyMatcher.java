788a7c5a15870938cd0197ef0c2563cc2e0fa342
Richard Wilson <richardjwilson@users.noreply.github.com>
2014-05-07 21:52:36 +0100

    Add unit tests and implementation of LoggingCompositePropertyMatcher.
    This now uses TextSubstitutingDescription to replace the default description
    with one that has no newlines and is therefore more suitable to log output.
    The test code uses a technique described in
    http://jsoftbiz.wordpress.com/2011/11/29/unit-testing-asserting-that-a-line-was-logged-by-logback/
    to test log output.

--- src/main/java/com/mistraltech/smog/core/LoggingCompositePropertyMatcher.java
+++ src/main/java/com/mistraltech/smog/core/LoggingCompositePropertyMatcher.java
@@ -1,5 +1,7 @@
 package com.mistraltech.smog.core;
 
+import org.hamcrest.Description;
+import org.hamcrest.StringDescription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,12 +10,19 @@ import org.slf4j.LoggerFactory;
  *
  * @param <T> type of matchable target object
  */
-public abstract class LoggingCompositePropertyMatcher<T> extends CompositePropertyMatcher<T> {
+public class LoggingCompositePropertyMatcher<T> extends CompositePropertyMatcher<T> {
     private static final Logger LOGGER = LoggerFactory.getLogger(CompositePropertyMatcher.class);
     private static final String LOGGING_CONJUNCTIVE_ADVERB = " and ";
 
     protected LoggingCompositePropertyMatcher(String matchedObjectDescription) {
         super(matchedObjectDescription);
+    }
+
+    @Override
+    protected Description createLogMismatchDescription() {
+        Description description = new TextSubstitutingDescription(MatchAccumulator.MISMATCH_CONJUNCTIVE_ADVERB, LOGGING_CONJUNCTIVE_ADVERB);
+        description.appendText(String.format("%s didn't match - ", this.getClass().getName()));
+        return description;
     }
 
     @Override
